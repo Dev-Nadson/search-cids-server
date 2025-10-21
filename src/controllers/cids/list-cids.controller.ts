1228265e55160e1473f9b1b7925d182a22f0f859
Dev-Nadson <nadson.pros@gmail.com>
2025-10-21 17:28:18 -0300

    Fix: load dos cids corrigido

--- src/controllers/cids/list-cids.controller.ts
+++ src/controllers/cids/list-cids.controller.ts
@@ -1,11 +1,10 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import * as fs from "fs"
 
-async function list_cids_controller(req: FastifyRequest, reply: FastifyReply) {
-    let data = fs.readFileSync('src/data/json/cids.json', 'utf-8')
-    data = JSON.parse(data)
+const cids = JSON.parse(fs.readFileSync('src/data/json/cids.json', 'utf-8'))
 
-    reply.status(200).send({ Cids: data })
+async function list_cids_controller(req: FastifyRequest, reply: FastifyReply) {
+    reply.status(200).send({ Cids: cids })
 }
 
 export { list_cids_controller }
