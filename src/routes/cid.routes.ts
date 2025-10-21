e7e83c5285016f0185ed6b7a1ea5e9b076083a4b
Dev-Nadson <nadson.pros@gmail.com>
2025-10-21 17:47:44 -0300

    Fix: nome das rotas corrigidas

--- src/routes/cid.routes.ts
+++ src/routes/cid.routes.ts
@@ -2,7 +2,7 @@ import type { FastifyInstance } from "fastify";
 import { list_cids_controller } from "../controllers/cids/list-cids.controller.js";
 
 async function cid_routes(app: FastifyInstance) {
-    app.get("/", list_cids_controller)
+    app.get("/cids", list_cids_controller)
 }
 
 export { cid_routes }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import type { FastifyInstance } from "fastify";
 import { cid_routes } from "./cid.routes.js";
 
 async function app_routes(app: FastifyInstance) {
-    app.register(cid_routes, { prefix: "/cids/" })
+    app.register(cid_routes)
 }
 
 export { app_routes }
