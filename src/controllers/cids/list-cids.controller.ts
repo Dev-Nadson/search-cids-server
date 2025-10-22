90bd20fd2e49a5b3f11e73a9b62d79b1e07d83c2
Dev-Nadson <nadson.pros@gmail.com>
2025-10-22 18:32:20 -0300

    Feat: env e CORS adicionados

--- src/controllers/cids/list-cids.controller.ts
+++ src/controllers/cids/list-cids.controller.ts
@@ -4,7 +4,7 @@ import * as fs from "fs"
 const cids = JSON.parse(fs.readFileSync('src/data/json/cids.json', 'utf-8'))
 
 async function list_cids_controller(req: FastifyRequest, reply: FastifyReply) {
-    reply.status(200).send({ Cids: cids })
+    reply.status(200).send(cids)
 }
 
 export { list_cids_controller }
