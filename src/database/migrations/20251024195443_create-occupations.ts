685526556b2fc14a3b915a19830f53eb8c3784cf
Dev-Nadson <nadson.pros@gmail.com>
2025-10-27 11:51:09 -0300

    Colunas atualizadas e corrigidas

--- src/database/migrations/20251024195443_create-occupations.ts
+++ src/database/migrations/20251024195443_create-occupations.ts
@@ -1,17 +1,14 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.createTable("occupations", (table) => {
-        table.increments("id").primary().notNullable()
-        table.string("occupation_code", 6).notNullable()
-        table.string("name").notNullable()
-        table.string("competence_date", 6).notNullable()
-    })
+        table.increments("id").primary().notNullable();
+        table.string("occupation_code", 6).notNullable().unique();
+        table.string("name", 150).notNullable();
+        table.index("occupation_code");
+    });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
-    await knex.schema.dropTable("occupations")
+    await knex.schema.dropTable("occupations");
 }
-
