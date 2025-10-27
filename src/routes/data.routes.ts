8300c81c15ba6b718dc05432ccd5674e4d863316
Dev-Nadson <nadson.pros@gmail.com>
2025-10-27 17:19:52 -0300

    Feat: controllers adicionados

--- src/routes/data.routes.ts
+++ src/routes/data.routes.ts
@@ -1,14 +1,16 @@
 import type { FastifyInstance } from "fastify";
 import { list_cids_controller } from "../controllers/list-cids.controller.js";
 import { list_procedures_controller } from "../controllers/list-procedures.controller.js";
+import { list_occupations_controller } from "../controllers/list-occupations.controller.js";
 import { list_cids_procedures_controller } from "../controllers/list-cids-procedures.controller.js";
+import { list_occupations_procedures_repository } from "../repositories/occupatios-procedures.repo.js";
 
 async function data_routes(app: FastifyInstance) {
     app.get("/cids", list_cids_controller)
     app.get("/procedures", list_procedures_controller)
-    app.get("/occupations", list_cids_controller)
+    app.get("/occupations", list_occupations_controller)
     app.get("/cids-procedures", list_cids_procedures_controller)
-    app.get("/occupatios-procedures", list_cids_controller)
+    app.get("/occupations-procedures", list_occupations_procedures_repository)
 }
 
 export { data_routes }
