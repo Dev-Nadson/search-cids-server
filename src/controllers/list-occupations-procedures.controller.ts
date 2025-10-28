aca7b9032f0094466ef4cd5a5019a72a98053e64
Dev-Nadson <nadson.pros@gmail.com>
2025-10-28 11:46:21 -0300

    Fix: identação corrigida

--- src/controllers/list-occupations-procedures.controller.ts
+++ src/controllers/list-occupations-procedures.controller.ts
@@ -4,7 +4,6 @@ import { querySchema } from "../schemas/page.schema.js";
 
 async function list_occupations_procedures_controller(req: FastifyRequest, reply: FastifyReply) {
     const { page, limit } = querySchema.parse(req.query)
-
     const data = await list_occupations_procedures_repository({ page, limit })
     reply.status(200).send(data)
 }
