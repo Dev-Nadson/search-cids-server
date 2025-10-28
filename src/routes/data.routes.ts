d7557a07f1209383fca0c9b4cbb3ca765da28dd6
Dev-Nadson <nadson.pros@gmail.com>
2025-10-28 11:35:30 -0300

    Fix: repo substituido por controller

--- src/routes/data.routes.ts
+++ src/routes/data.routes.ts
@@ -3,14 +3,14 @@ import { list_cids_controller } from "../controllers/list-cids.controller.js";
 import { list_procedures_controller } from "../controllers/list-procedures.controller.js";
 import { list_occupations_controller } from "../controllers/list-occupations.controller.js";
 import { list_cids_procedures_controller } from "../controllers/list-cids-procedures.controller.js";
-import { list_occupations_procedures_repository } from "../repositories/occupatios-procedures.repo.js";
+import { list_occupations_procedures_controller } from "../controllers/list-occupations-procedures.controller.js";
 
 async function data_routes(app: FastifyInstance) {
     app.get("/cids", list_cids_controller)
     app.get("/procedures", list_procedures_controller)
     app.get("/occupations", list_occupations_controller)
     app.get("/cids-procedures", list_cids_procedures_controller)
-    app.get("/occupations-procedures", list_occupations_procedures_repository)
+    app.get("/occupations-procedures", list_occupations_procedures_controller)
 }
 
 export { data_routes }
