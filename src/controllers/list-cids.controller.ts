bfeffffe4e3bd8e51d61f28c2cc29b255a7dd6b7
Dev-Nadson <nadson.pros@gmail.com>
2025-10-28 18:06:38 -0300

    Fix: identação corrigida

--- src/controllers/list-cids.controller.ts
+++ src/controllers/list-cids.controller.ts
@@ -6,7 +6,6 @@ async function list_cids_controller(req: FastifyRequest, reply: FastifyReply) {
     const { page, limit, term } = querySchema.parse(req.query)
     const data = await list_cids_repository({ page, limit, term })
     reply.status(200).send(data)
-
 }
 
 export { list_cids_controller }
