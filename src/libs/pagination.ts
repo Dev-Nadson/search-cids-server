59ffa8e1e5c7016ec5012d7da1b5a6207e8d6ab8
Dev-Nadson <nadson.pros@gmail.com>
2025-10-29 10:36:57 -0300

    Feat: search otimizada

--- src/libs/pagination.ts
+++ src/libs/pagination.ts
@@ -1,21 +1,22 @@
 import { Knex } from "../database/config.js";
-import type { queryType } from "../schemas/page.schema.js";
+import type { queryType, searchType } from "../schemas/page.schema.js";
 
-async function pagination(table_name: string, pages: queryType, identifier?: string, select_properties?: string[]) {
+async function pagination(table_name: string, pages: queryType, search_params?: searchType, select_properties?: string[]) {
     const { page, limit, term } = pages
     const offset = (page - 1) * limit
 
     let countQuery = Knex(table_name)
     let dataQuery = Knex(table_name)
 
-    if (term && identifier) {
-        countQuery = countQuery.where(identifier, "LIKE", `%${term}%`),
-            dataQuery = dataQuery.where(identifier, "LIKE", `%${term}%`)
+    if (term && search_params) {
+        const { name, code } = search_params
+        countQuery = countQuery.where(name, "LIKE", `%${term}%`).orWhere(code, "LIKE", `${term}`),
+            dataQuery = dataQuery.where(name, "LIKE", `%${term}%`).orWhere(code, "LIKE", `${term}`)
     }
 
     const [count, data] = await Promise.all([
         countQuery.count("* as total").first(),
-        dataQuery.select(select_properties || "").limit(limit).offset(offset)
+        dataQuery.select(select_properties || "*").limit(limit).offset(offset)
     ])
 
     const total = Number(count?.total ?? 0)
