68a192df81d9d9053aa66c96130a7423c480b980
Dev-Nadson <nadson.pros@gmail.com>
2025-10-29 11:25:56 -0300

    Feat: search adicionada

--- src/repositories/list-cids-procedures.repo.ts
+++ src/repositories/list-cids-procedures.repo.ts
@@ -1,8 +1,13 @@
 import { pagination } from "../libs/pagination.js";
-import type { queryType } from "../schemas/page.schema.js";
+import type { queryType, searchType } from "../schemas/page.schema.js";
 
 async function list_cids_procedures_repository(pages: queryType) {
-    const data = await pagination("cids_procedures", pages)
+    const searchParams: searchType = {
+        code: "procedure_code",
+        param: "cid_code"
+    }
+
+    const data = await pagination("cids_procedures", pages, searchParams)
     return data
 }
 
--- src/repositories/list-occupatios-procedures.repo.ts
+++ src/repositories/list-occupatios-procedures.repo.ts
@@ -1,8 +1,13 @@
 import { pagination } from "../libs/pagination.js";
-import type { queryType } from "../schemas/page.schema.js";
+import type { queryType, searchType } from "../schemas/page.schema.js";
 
 async function list_occupations_procedures_repository(pages: queryType) {
-    const data = await pagination("occupations_procedures", pages)
+    const searchParams: searchType = {
+        code: "procedure_code",
+        param: "occupation_code"
+    }
+
+    const data = await pagination("occupations_procedures", pages, searchParams)
     return data
 }
 
